e123817442389dd309f8659b5ed2661c7089dda2
niostack <niostack@gmail.com>
2024-01-31 12:08:09 +0800

    Update package version and description

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,20 @@
 fn main() {
-    tauri_build::build()
+    let mut windows = tauri_build::WindowsAttributes::new();
+    if std::env::var("PRODUCTION").is_ok() {
+        windows = windows.app_manifest(
+            r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+      <security>
+          <requestedPrivileges>
+              <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+          </requestedPrivileges>
+      </security>
+  </trustInfo>
+</assembly>
+"#,
+        );
+    }
+    let attrs = tauri_build::Attributes::new().windows_attributes(windows);
+    tauri_build::try_build(attrs).expect("failed to run build script");
 }
