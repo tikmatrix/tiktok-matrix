55dd0cd24021f18db076d4da22426399de34d8fe
niostack <niostack@gmail.com>
2024-03-30 10:16:30 +0800

    u

--- merge.bat
+++ merge.bat
@@ -1,4 +1,6 @@
 echo merging origin main to current branch
+git add .
+git commit -m "merge origin main to current branch"
 git fetch origin main
 git merge origin/main
 git submodule foreach --recursive git fetch origin
