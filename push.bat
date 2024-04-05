21640ada4a28720db0c442559830eeb937db2b52
niostack <niostack@gmail.com>
2024-04-05 15:41:23 +0800

    commit by auto

--- push.bat
+++ push.bat
@@ -2,6 +2,6 @@ echo committing
 git add .
 git commit -a -m "commit by auto"
 git push
-git submodule foreach --recursive git add .
-git submodule foreach --recursive git commit -a -m "commit by auto" || echo "nothing to commit"
-git submodule foreach --recursive git push
+git submodule foreach git add .
+git submodule foreach git commit -a -m "commit by auto" || echo "nothing to commit"
+git submodule foreach git push
