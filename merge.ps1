65424f41f8136e496a2ba6a85b74b5b23ada4a52
niostack <niostack@gmail.com>
2024-04-16 22:51:38 +0800

    job

--- merge.ps1
+++ merge.ps1
@@ -1,7 +1,7 @@
 echo merging origin main to current branch
 git add .
 git commit -m "merge origin main to current branch"
-git fetch origin main
-git merge origin/main
 git submodule foreach git fetch origin
 git submodule foreach git merge origin/main
+git fetch origin main
+git merge origin/main
