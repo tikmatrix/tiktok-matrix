ed59467cf854f54398663b20871daa5567a55dc3
niostack <niostack@gmail.com>
2024-06-27 12:04:00 +0800

    fix

--- merge.ps1
+++ merge.ps1
@@ -2,6 +2,6 @@ echo merging origin main to current branch
 git add .
 git commit -m "merge origin main to current branch"
 git submodule foreach git fetch origin
-git submodule foreach git merge origin/main
+git submodule foreach git merge origin/main -m "Merge branch 'main' into feature-branch"
 git fetch origin main
-git merge origin/main
+git merge origin/main -m "Merge branch 'main' into feature-branch"
