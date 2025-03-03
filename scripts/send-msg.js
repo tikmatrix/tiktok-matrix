f176fe0e5103e56525fda25b8efa36a965e50d21
niostack <niostack@gmail.com>
2025-03-03 14:45:58 +0800

    Update CHANGELOG formatting and Telegram messaging script

--- scripts/send-msg.js
+++ scripts/send-msg.js
@@ -1,22 +1,18 @@
-const chatId = process.env.CHAT_ID
-const token = process.env.TOKEN
-const url = 'https://pro.api.tikmatrix.com/front-api/download'
+const chatId = process.env.CHAT_ID;
+const token = process.env.TOKEN;
+const url = 'https://pro.api.tikmatrix.com/front-api/download';
 
 import TelegramBot from 'node-telegram-bot-api';
-// 从CHANGELOG.md中读取版本更新说明
 import fs from 'fs';
-const configPath = "src-tauri/tauri.conf.json"
-const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'))
+const configPath = "src-tauri/tauri.conf.json";
+const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
 const changelog = fs.readFileSync('CHANGELOG', 'utf8');
-const message = `*${config.package.productName} v${config.package.version}* New Release!\n\n${changelog}\n\n*Please update to the latest version*\n[Download Here](${url})`
+const message = `*${config.package.productName} v${config.package.version}* New Release!\n\n\`\`\`\n${changelog}\n\`\`\`\n\n*Please update to the latest version*\n[Download Here](${url})`;
 
-console.log(message)
+console.log(message);
 
-// 创建一个机器人实例
 const bot = new TelegramBot(token, { polling: false });
 
-
-// 发送消息
 bot.sendMessage(chatId, message, { parse_mode: 'Markdown' })
     .then(response => {
         console.log('Message sent successfully:', response);
