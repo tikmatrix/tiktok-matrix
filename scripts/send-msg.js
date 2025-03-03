9bb6362b94afc777e62734fdb7eed66ea504d97d
niostack <niostack@gmail.com>
2025-03-03 14:21:25 +0800

    Simplify Telegram release messaging script and remove hardcoded environment variables

--- scripts/send-msg.js
+++ scripts/send-msg.js
@@ -1,8 +1,6 @@
-process.env.URL = 'https://pro.api.tikmatrix.com/front-api/download'
 const chatId = process.env.CHAT_ID
 const token = process.env.TOKEN
-const tag = process.env.TAG
-const url = process.env.URL
+const url = 'https://pro.api.tikmatrix.com/front-api/download'
 
 import TelegramBot from 'node-telegram-bot-api';
 // 从CHANGELOG.md中读取版本更新说明
@@ -10,8 +8,10 @@ import fs from 'fs';
 const configPath = "src-tauri/tauri.conf.json"
 const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'))
 const changelog = fs.readFileSync('CHANGELOG', 'utf8');
-// const message = `A new release: ${tag} is available:\n ${changelog}\n Check it out at ${url}`
-const message = `*${config.package.productName} v${config.package.version}* New Release!\n\n${changelog}\n\n*Please update to the latest version*\n[Download Here](${url})`
+const message = `*${config.package.productName} v${config.package.version}* New Release!\n
+${changelog}\n
+*Please update to the latest version*\n
+[Download Here](${url})`
 
 
 // 创建一个机器人实例
