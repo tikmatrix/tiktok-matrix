d89549cb01f4aba353a7d20d1463e29b4fa5f4e4
niostack <niostack@gmail.com>
2025-04-22 11:43:34 +0800

    1

--- scripts/update-version_v2.js
+++ scripts/update-version_v2.js
@@ -3,7 +3,7 @@ const configPath = "src-tauri/tauri.conf.json"
 const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'))
 const signaturePath = `src-tauri/target/release/bundle/msi/TikMatrix_${config.package.version}_x64_en-US.msi.zip.sig`
 const signature = fs.readFileSync(signaturePath, 'utf-8')
-const updateJson = {
+let body = JSON.stringify({
     "version": `v${config.package.version}`,
     "notes": `v${config.package.version} is released! Please update to the new version.`,
     "pub_date": new Date().toISOString(),
@@ -17,28 +17,41 @@ const updateJson = {
             "url": `https://r2.tikmatrix.com/TikMatrix_${config.package.version}_x64_en-US.dmg`
         }
     }
-}
-const updateJsonStr = JSON.stringify(updateJson, null, 2)
-const response1 = await fetch('https://pro.api.tikmatrix.com/ci/update_version_info', {
+}, null, 2)
+let response = await fetch('https://pro.api.tikmatrix.com/ci/update_version_info', {
     method: 'PUT',
     headers: {
         'Content-Type': 'text/plain',
-        'Content-Length': updateJsonStr.length,
+        'Content-Length': body.length,
         'Authorization': 'Bearer ' + process.env.API_KEY
     },
-    body: updateJsonStr
+    body: body
 })
-console.log(`update_version_info: ${response1.status} ${response1.statusText}`)
-//update download url
-const downloadUrl = `https://r2.tikmatrix.com/TikMatrix_${config.package.version}_x64_en-US.msi`
+console.log(`update_version_info: ${response.status} ${response.statusText}`)
+//update windows download url
+body = `https://r2.tikmatrix.com/TikMatrix_${config.package.version}_x64_en-US.msi`
 //put https://pro.api.tikmatrix.com/download
-const response2 = await fetch('https://pro.api.tikmatrix.com/ci/update_download_url', {
+response = await fetch('https://pro.api.tikmatrix.com/ci/update_download_url', {
     method: 'PUT',
     headers: {
         'Content-Type': 'text/plain',
-        'Content-Length': downloadUrl.length,
-        'Authorization': 'Bearer ' + process.env.API_KEY
+        'Content-Length': body.length,
+        'Authorization': 'Bearer ' + process.env.API_KEY,
+        'X-Platform': 'windows'
     },
-    body: downloadUrl
+    body: body
 })
-console.log(`update_download_url: ${response2.status} ${response2.statusText}`)
+console.log(`update_download_url: ${response.status} ${response.statusText}`)
+//update mac download url
+body = `https://r2.tikmatrix.com/TikMatrix_${config.package.version}_x64_en-US.dmg`
+response = await fetch('https://pro.api.tikmatrix.com/ci/update_download_url', {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'text/plain',
+        'Content-Length': body.length,
+        'Authorization': 'Bearer ' + process.env.API_KEY,
+        'X-Platform': 'mac'
+    },
+    body: body
+})
+console.log(`update_download_url: ${response.status} ${response.statusText}`)
