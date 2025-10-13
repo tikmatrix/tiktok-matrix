e01342b9595ff31c28f72c9d0d7545ee36e45699
niostack <niostack@gmail.com>
2025-10-13 22:23:42 +0800

    feat: 将Telegram和WhatsApp支持按钮从button更改为链接，优化用户体验

--- src/components/pricing/ContactSupport.vue
+++ src/components/pricing/ContactSupport.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="flex items-center gap-2">
-        <button class="btn btn-outline btn-xs btn-info shrink whitespace-nowrap"
+        <a class="btn btn-outline btn-xs btn-info shrink whitespace-nowrap"
             :href="whitelabelConfig.branding?.telegramSupport" target="_blank"
             v-if="whitelabelConfig.branding?.telegramSupport">
             <font-awesome-icon icon="fab fa-telegram" class="text-sm" />
             {{ $t('telegramSupport') }}
-        </button>
-        <button class="btn btn-outline btn-xs btn-success shrink whitespace-nowrap"
+        </a>
+        <a class="btn btn-outline btn-xs btn-success shrink whitespace-nowrap"
             :href="whitelabelConfig.branding?.whatsappSupport" target="_blank"
             v-if="whitelabelConfig.branding?.whatsappSupport">
             <font-awesome-icon icon="fab fa-whatsapp" class="text-sm" />
             {{ $t('whatsappSupport') }}
-        </button>
+        </a>
         <button class="btn btn-outline btn-xs btn-secondary shrink" @click="copyEmail"
             v-if="whitelabelConfig.branding?.emailSupport">
             <font-awesome-icon icon="fas fa-envelope" class="text-sm" />
