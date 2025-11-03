95dd7465db9dc67d4509a91fecb0bdc86ee69220
niostack <niostack@gmail.com>
2025-11-03 15:18:51 +0800

    修复 massDM 按钮的条件渲染，移除对 whitelabelConfig.targetApp 的依赖

--- src/components/Scripts.vue
+++ src/components/Scripts.vue
@@ -61,7 +61,7 @@
         <font-awesome-icon icon="fa-solid fa-video" class="h-3 w-3 text-success" />
         {{ $t('boostLives') }}
     </button>
-    <button class="btn btn-md btn-primary  ml-1 mb-1" v-if="whitelabelConfig.targetApp === 'tiktok'"
+    <button class="btn btn-md btn-primary  ml-1 mb-1"
         @click="$emiter('showDialog', { name: 'beforeRunScriptDialog', script: { name: 'massDM' } })">
         <font-awesome-icon icon="fa-solid fa-message" class="h-3 w-3" />
         {{ $t('massDM') }}
